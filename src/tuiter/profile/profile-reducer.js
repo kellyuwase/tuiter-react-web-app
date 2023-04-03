9054c43b292ef7b54c42bebcbfe9b08fecc4d0c4
feliciteuwase <felciteuwase98@gmail.com>
2023-04-02 20:20:45 -0400

    new banner

--- src/tuiter/profile/profile-reducer.js
+++ src/tuiter/profile/profile-reducer.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const profileSlice = createSlice({
   name: "profile",
   initialState: {
-    name: 'Jose Annunziato', handle: '@jannunzi',
-    profilePicture: 'jose.jpeg', bannerPicture: 'polyglot.jpeg',
+    name: 'SpongeBob', handle: '@spongebob',
+    profilePicture: 'spongebob.jpeg', bannerPicture: 'spongebanner.jpeg',
     bio: 'Faculty, Software Engineer, AI, Space, and renewable enthusiast. Retuits and likes are not endorsements.',
     location: 'Boston, MA', dateOfBirth: '1968-07-07', dateJoined: '2009-04-02',
     followingCount: 340, followersCount: 223, total_tuits: 6114,
